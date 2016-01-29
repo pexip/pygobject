8e711822e612d75a5be2afe4dcfb5b4238e352c8
Mikhail Fludkov <misha@pexip.com>
2016-01-29 09:01:12 +0100

    GObject: use mutex instead of boolean

--- gi/overrides/GObject.py
+++ gi/overrides/GObject.py
@@ -22,6 +22,7 @@
 # USA
 
 import sys
+import mutex
 import warnings
 from collections import namedtuple
 
@@ -211,17 +212,16 @@ __all__ += ['features', 'list_properties', 'new',
 
 class Value(GObjectModule.Value):
     def __init__(self, value_type=None, py_value=None):
+        self._del_mutex = mutex.mutex()
         GObjectModule.Value.__init__(self)
-        self._del_called = False
         if value_type is not None:
             self.init(value_type)
             if py_value is not None:
                 self.set_value(py_value)
 
     def __del__(self):
-        if not self._del_called and self._free_on_dealloc and self.g_type != TYPE_INVALID:
+        if self.del_mutex.testandset() and self._free_on_dealloc and self.g_type != TYPE_INVALID:
             self.unset()
-        self._del_called = True
 
         # We must call base class __del__() after unset.
         super(Value, self).__del__()
